e79e9388d0da210a880ad0237bba1a8bdf4e4459
yuyan <112233@qq.com>
2021-06-22 21:13:44 +0800

    first commit

---
+++ src/main/java/com/yy/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.yy.git;
+
+/**
+ * @author emm
+ * @data 2021/6/22 21:10
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
